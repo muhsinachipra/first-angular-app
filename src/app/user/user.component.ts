1abbc866f7a5f785dc87ae2b1971f8db393f1dcd
muhsin <muhsin@zoptal.in>
2025-11-08 11:01:27 +0530

    feat: implement user management features with header and user components

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+
+  get userImage(): string {
+    return 'assets/users/' + this.avatar;
+  }
+
+  onUserClick() {
+  }
+}
